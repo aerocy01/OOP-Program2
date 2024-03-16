a3caa19682a799db35ff8490f13d022d65077258
aerocy01 <163655245+aerocy01@users.noreply.github.com>
2024-03-16 22:26:45 +0800

    main.py
    OOP-Program2

---
+++ main.py
@@ -0,0 +1,6 @@
+# Get the user's name and dream job
+name = input("Please enter your name: ")
+dream_job = input("Please enter your dream job: ")
+
+# Print the name and dream job in a fancy way
+print(f"Hi there, {name.title()}! It's great to meet you.\n Your dream job is to be a {dream_job.title()}, you must be very passionate about it!)
